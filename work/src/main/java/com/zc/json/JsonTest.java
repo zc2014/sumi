a8ce2d552a86276b1b014b2825ce57c0288fd84d
周聪 <zhoucong04@meituan.com>
2019-11-10 22:41:15 +0800

    remove netty_lecture

--- work/src/main/java/com/zc/json/JsonTest.java
+++ work/src/main/java/com/zc/json/JsonTest.java
@@ -15,8 +15,8 @@ import java.util.LinkedHashMap;
 public class JsonTest {
     public static void main(String[] args) {
 //        String message = "[{\"name\":\"wm_product_name\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"雀巢金牌咖啡(瓶装) 100g\"},{\"name\":\"wm_product_description\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"咖啡的醇香和牛奶的甘甜美妙交融\"},{\"name\":\"wm_product_unit\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"份\"},{\"name\":\"wm_product_spec\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"100g\"}]";
-//        String message = "\t[{\"name\":\"wm_product_name\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"顺鑫鑫源烧烤牛板筋袋装85g\"},{\"name\":\"wm_product_unit\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"份\"}]";
-        String message = "[{\"name\":\"wm_product_name\",\"type\":\"1\",\"value\":\"情趣内衣三点猫女透视兔女郎制服极诱惑39号\"}]\t";
+        String message = "\t[{\"name\":\"wm_product_name\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"顺鑫鑫源烧烤牛板筋袋装85g\"},{\"name\":\"wm_product_unit\",\"setName\":true,\"setType\":true,\"setValue\":true,\"type\":\"1\",\"value\":\"份\"}]";
+//        String message = "[{\"name\":\"wm_product_name\",\"type\":\"1\",\"value\":\"情趣内衣三点猫女透视兔女郎制服极诱惑39号\"}]\t";
 
         for (int i = 0; i < 1; i++) {
             String data = JSON.toJSONString(message);
@@ -33,6 +33,15 @@ public class JsonTest {
             array.add(jsonObject);
         });
         System.out.println(array.toJSONString());
+        JSONArray array1 = new JSONArray();
+
+        array.forEach(data -> {
+            LinkedHashMap<String, Object> json = JSON.parseObject(((JSONObject)data).toJSONString(), LinkedHashMap.class, Feature.OrderedField);
+            JSONObject jsonObject = new JSONObject(true);
+            jsonObject.putAll(json);
+            array1.add(jsonObject);
+        });
+        System.out.println(array1.toJSONString());
 
 //        LinkedHashMap<String, Object> json = JSON.parseObject(((JSONObject) JSON.parseArray(message).get(0)).toJSONString(), LinkedHashMap.class, Feature.OrderedField);
 //        JSONObject jsonObject = new JSONObject(true);
@@ -43,6 +52,9 @@ public class JsonTest {
 //            System.out.println(array.toJSONString());
 //        }
 
+        new Thread(() ->{
+
+        });
 
     }
 }
